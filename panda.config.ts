20de0cce92de02f4bb77f05d8245c3a642a8a85b
trasta <trasta298@gmail.com>
2023-10-14 23:44:55 +0900

    🐛 panda css

--- panda.config.ts
+++ panda.config.ts
@@ -5,7 +5,7 @@ export default defineConfig({
     preflight: true,
     
     // Where to look for your css declarations
-    include: ["./src/**/*.{js,jsx,ts,tsx}", "./pages/**/*.{js,jsx,ts,tsx}"],
+    include: ["./src/**/*.{js,jsx,ts,tsx,astro}", "./pages/**/*.{js,jsx,ts,tsx,astro}"],
 
     // Files to exclude
     exclude: [],
