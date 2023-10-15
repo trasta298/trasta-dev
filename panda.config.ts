d956632a24351e027134ee6edc18279e475588a1
trasta <trasta298@gmail.com>
2023-10-15 13:38:17 +0900

    💚 formatter

--- panda.config.ts
+++ panda.config.ts
@@ -1,41 +1,42 @@
-import { defineConfig } from "@pandacss/dev"
+import { defineConfig } from '@pandacss/dev'
 
 const theme = {
   tokens: {
     colors: {
       white: { value: '#fafafa' },
       black: { value: '#171717' },
-    }
+    },
   },
   semanticTokens: {
     colors: {
       bg: {
         value: { base: '{colors.white}', _dark: '{colors.black}' },
-        description: 'Background color'
+        description: 'Background color',
       },
       text: {
         value: { base: '{colors.black}', _dark: '{colors.white}' },
-        description: 'Text color'
+        description: 'Text color',
       },
-    }
-  }
+    },
+  },
 }
 
-
 export default defineConfig({
-    // Whether to use css reset
-    preflight: true,
-    
-    // Where to look for your css declarations
-    include: ["./src/**/*.{js,jsx,ts,tsx,astro}", "./pages/**/*.{js,jsx,ts,tsx,astro}"],
+  // Whether to use css reset
+  preflight: true,
 
-    // Files to exclude
-    exclude: [],
+  // Where to look for your css declarations
+  include: [
+    './src/**/*.{js,jsx,ts,tsx,astro}',
+    './pages/**/*.{js,jsx,ts,tsx,astro}',
+  ],
 
-    // Useful for theme customization
-    theme: theme,
+  // Files to exclude
+  exclude: [],
 
-    // The output directory for your css system
-    outdir: "styled-system",
+  // Useful for theme customization
+  theme: theme,
 
+  // The output directory for your css system
+  outdir: 'styled-system',
 })
