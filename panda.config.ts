d85accb8e407f907a38d9b704d1a64b366cdf2f7
trasta <trasta298@gmail.com>
2023-10-18 00:37:23 +0900

    ⚡️ who am i

--- panda.config.ts
+++ panda.config.ts
@@ -7,6 +7,7 @@ const theme = {
       black: { value: '#171717' },
       gray: { value: '#e5e5e5' },
       dark: { value: '#262626' },
+      primary: { value: '#f59e0b' },
     },
   },
   semanticTokens: {
@@ -30,8 +31,66 @@ const theme = {
       spin: {
         to: { transform: 'rotate(360deg)' },
       },
+    },
+  },
+}
+
+const globalCss  = {
+  'h1': {
+    'font-size': '2.2rem',
+    'font-weight': '700',
+    'margin': '30px 0 24px',
+    'line-height': '1.7',
+    'border-bottom': {
+      'base': '1px solid gray',
+      '_dark': '1px solid dark',
     }
-  }
+  },
+  'h2': {
+    'font-size': '1.8rem',
+    'font-weight': '700',
+    'margin': '20px 0 14px',
+  },
+  'h3': {
+    'font-size': '1.4rem',
+    'font-weight': '700',
+    'margin': '18px 0 12px',
+  },
+  'h4': {
+    'font-size': '1.3rem',
+    'font-weight': '700',
+    'margin': '16px 0 10px',
+  },
+  'h5': {
+    'font-size': '1.2rem',
+    'font-weight': '700',
+    'margin': '8px 0 6px',
+  },
+  'p': {
+    'font-size': '1rem',
+    'line-height': '1.6',
+    'margin': '0 0 16px',
+  },
+  'a': {
+    'color': 'primary',
+    'text-decoration': 'none',
+  },
+  'pre': {
+    'margin': '16px 0',
+    'padding': '16px',
+    'border-radius': '6px',
+  },
+  'code' : {
+    'font-family': `Consolas, 'Courier New', Courier, Monaco, monospace`,
+  },
+  'ul' : {
+    'padding-left': '25px',
+    'list-style-type': 'disc'
+  },
+  'ol' : {
+    'padding-left': '25px',
+    'list-style-type': 'decimal'
+  },
 }
 
 export default defineConfig({
@@ -52,4 +111,6 @@ export default defineConfig({
 
   // The output directory for your css system
   outdir: 'styled-system',
+
+  globalCss: globalCss,
 })
