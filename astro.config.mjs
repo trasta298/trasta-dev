92b293f92625e27e6d140fcd4101b3605fa3acd1
trasta <trasta298@gmail.com>
2023-11-11 01:19:47 +0900

    ⚡️ add gtag

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,18 @@
 import { defineConfig } from 'astro/config';
 import pandacss from '@pandacss/astro';
+import partytown from "@astrojs/partytown";
 
 import mdx from "@astrojs/mdx";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [pandacss(), mdx()]
+  integrations: [
+    pandacss(),
+    mdx(),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ]
 });
