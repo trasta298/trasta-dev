6ebcde5df599825a01939e27ba46e8fa2a354fce
trasta <trasta298@gmail.com>
2023-11-13 10:06:32 +0900

    ⚡️ Add sitemap integration and link to Layout.astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,15 @@
 import { defineConfig } from 'astro/config';
 import pandacss from '@pandacss/astro';
 import partytown from "@astrojs/partytown";
-
 import mdx from "@astrojs/mdx";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [
-    pandacss(),
-    mdx(),
-    partytown({
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ]
+  integrations: [pandacss(), mdx(), partytown({
+    config: {
+      forward: ["dataLayer.push"]
+    }
+  }), sitemap()]
 });
